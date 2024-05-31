dba2a8a60c18142f8203e1baae722432da6773c8
Matías Lecaros <matiaslecarost@uc.cl>
2024-05-31 18:24:58 -0400

    feat(MainPage): Ads added

--- src/components/Products/ProductSlider.jsx
+++ src/components/Products/ProductSlider.jsx
@@ -55,33 +55,32 @@ function ProductSlider({productItems}){
             {
                 breakpoint: 1440,
                 settings: {
-                slidesToShow: 4,
-                dots: true,
+                    slidesToShow: 4,
                 },
             },
             {
                 breakpoint: 1024,
                 settings: {
-                slidesToShow: 3,
-                dots: true,
+                    slidesToShow: 3,
                 },
             },
             {
                 breakpoint: 768,
                 settings: {
-                slidesToShow: 2,
-                dots: true,
+                    variableWidth: true,
+                    slidesToShow: 2,
                 },
             },
             {
                 breakpoint: 576,
                 settings: {
-                slidesToShow: 1,
-                dots: true,
+                    variableWidth: true,
+                    slidesToShow: 1,
                 },
             },
-            ],
+        ],
     };
+
 
     return (
     <>
--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,5 +1,6 @@
 import ProductsData from "../../components/Products/ProductsData";
 import ProductSlider from "../../components/Products/ProductSlider";
+import AdSlider from "../../components/Ads/AdSlider";
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import './MainPage.css';
@@ -8,22 +9,28 @@ function MainPage(){
     const { productItems } = ProductsData;
     console.log(productItems);
   return (
-    <>
-    <div>
-      <h1>Main Page</h1>
-    </div>
-    <div className="heading">
-        <i className="fa fa-bolt"></i>
-        <h2>Ofertas Destacadas</h2>
-    </div>
-    <ProductSlider productItems={productItems}/>
-    <div className="heading">
-        <i className="fa fa-line-chart"></i>
-        <h2>Productos más vendidos</h2>
-    </div>
-    <ProductSlider productItems={productItems}/>
-
-    </>
-  );
+        <>
+            <div>
+                <h1>Main Page</h1>
+            </div>
+            <div className="ads">
+                <AdSlider />
+            </div>
+            <div className="heading">
+                <i className="fa fa-percent"></i>
+                <h2>Ofertas Destacadas</h2>
+            </div>
+            <div className="product-slider">
+                <ProductSlider productItems={productItems} />
+            </div>
+            <div className="heading">
+                <i className="fa fa-line-chart"></i>
+                <h2>Productos más vendidos</h2>
+            </div>
+            <div className="product-slider">
+                <ProductSlider productItems={productItems} />
+            </div>
+        </>
+    );
 }
 export default MainPage;
